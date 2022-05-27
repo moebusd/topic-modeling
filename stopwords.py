101413f364e67e70cbe60ccfc106ca2fb85504dc
moebusd <dennis.moebus@fernuni-hagen.de>
2022-05-27 08:52:09 +0200

    Create stopwords.py

---
+++ stopwords.py
@@ -0,0 +1,31 @@
+def remove_stopwords_by_list(data, stoplist):
+    data_out = [[word for word in line if word not in stoplist] for line in data]
+    return data_out
+
+
+def remove_stopwords_by_threshold(data, threshold):
+    wordcounts = {}
+    wordcount = 0
+
+    for line in data:
+        wordcount = wordcount + len(line)
+
+    for line in data:
+        for word in line:
+            if word not in wordcounts:
+                wordcounts[word] = 1
+            if word in wordcounts:
+                wordcounts[word] += 1
+
+    wordcounts_sorted = []
+
+    for word, count in wordcounts.items():
+        t = ((count / wordcount) * 100, count, word)
+        wordcounts_sorted.append(t)
+
+    wordcounts_out = sorted(wordcounts_sorted, reverse=True)
+
+    stoplist_by_threshold = [word[2] for word in wordcounts_out if word[0] > threshold]
+
+    data_out = [[word for word in line if word not in stoplist_by_threshold] for line in data]
+    return data_out
