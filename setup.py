ec3a7d1db1265d7898f0e509d5c000b8ed4b9804
moebusd <39922252+moebusd@users.noreply.github.com>
2022-05-27 12:11:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import urllib.request
 
 setup(
         name='topic_modeling',
-        version=urllib.request.urlopen("https://github.com/moebusd/topic-modeling/blob/main/version.txt"),
+        version=urllib.request.urlopen("https://github.com/moebusd/topic_modeling/blob/main/version.txt"),
         author="Dennis Möbus",
         author_email="dennis.moebus@fernuni-hagen.de",
-        url="https://github.com/moebusd/topic-modeling",
+        url="https://github.com/moebusd/topic_modeling",
         description="modules of topic modeling pipeline as developed by the research group digital humanities at FernUniversität in Hagen",
         python_requires=">=3.6",
     )
