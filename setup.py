c76fe1a6f259e328876de182798cad1ad474a674
moebusd <39922252+moebusd@users.noreply.github.com>
2022-05-28 08:29:33 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='topic_modeling',
+      version='0.6',
+      description='Topic Modeling Pipeline developed by DH research group at FernUniversität in Hagen',
+      url='https://github.com/moebusd/topic_modeling',
+      author='Dennis Möbus',
+      author_email='dennis.moebus@fernuni-hagen.de',
+      license='MIT',
+      packages=['topic_modeling'],
+      zip_safe=False)
