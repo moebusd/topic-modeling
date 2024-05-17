ef35328fa95cdc6cdcb1e40aa2028bf047be53de
moebusd <39922252+moebusd@users.noreply.github.com>
2024-05-17 08:37:52 +0200

    Update topic_training_new_mallet_wrapper.py

--- topic_modeling/topic_training_new_mallet_wrapper.py
+++ topic_modeling/topic_training_new_mallet_wrapper.py
@@ -5,7 +5,7 @@ from mallet_wrapper.coherencemodel import CoherenceModel
 import os
 from datetime import datetime
 import pandas as pd
-def topic_training_mallet(dataset, name_dataset, user, topics, mallet_path, optimize_interval_mallet=500, iterations_mallet=5000, random_seed_mallet=100, alpha=auto):
+def topic_training_mallet(dataset, name_dataset, user, topics, mallet_path, optimize_interval_mallet=500, iterations_mallet=5000, random_seed_mallet=100, alpha='auto'):
 
     id2word = corpora.Dictionary(dataset)
 
